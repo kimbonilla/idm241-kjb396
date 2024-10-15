38cdc55f10afc76df4499745e3510822ef1c5d4a
kimbonilla <147077374+kimbonilla@users.noreply.github.com>
2024-10-14 20:06:20 -0400

    add "saving..." status to button :zap:
    added the delay where it says "saving..." before the button status changes

--- idm241/alpha/alpha.js
+++ idm241/alpha/alpha.js
@@ -2,14 +2,20 @@
 const alphaBtn = document.querySelector(".alpha-button button");
 
 alphaBtn.addEventListener("click", function() {
-    if (alphaBtn.innerHTML==="Want to Read") {
-    alphaBtn.innerHTML = '<img src="checkmark.png" alt="checkmark"> Want to Read';
-    alphaBtn.style.backgroundColor = "var(--light-gray)";
-    alphaBtn.style.color = "var(--black)";
-    alphaBtn.title = "Remove from your to-read shelf.";
-    }else {
-        alphaBtn.innerHTML = "Want to Read";
-        alphaBtn.removeAttribute("style");
-        alphaBtn.title = "Add to your to-read shelf.";
-    }
+    alphaBtn.innerHTML = "saving...";
+    
+    setTimeout(function() {
+        if (alphaBtn.innerHTML === "saving...") {
+            if (alphaBtn.title === "Add to your to-read shelf.") {
+                alphaBtn.innerHTML = '<img src="checkmark.png" alt="checkmark"> Want to Read';
+                alphaBtn.style.backgroundColor = "var(--light-gray)";
+                alphaBtn.style.color = "var(--black)";
+                alphaBtn.title = "Remove from your to-read shelf.";
+            } else {
+                alphaBtn.innerHTML = "Want to Read";
+                alphaBtn.removeAttribute("style");
+                alphaBtn.title = "Add to your to-read shelf.";
+            }
+        }
+    }, 200);
 });
